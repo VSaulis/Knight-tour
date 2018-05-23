2b7260354b7969e2e448e6e7ba10eeb5c15a08d9
Vytautas Saulis <vsaulis@trackforce.com>
2018-05-23 21:57:13 +0300

    Add project files.

---
+++ KnightTour/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KnightTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
